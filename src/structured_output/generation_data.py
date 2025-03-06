40dcf72a8bcf2f8a49b3b2e37597df2e826c2a0a
Jeanne <jeanne.wwork@gmail.com>
2025-03-06 15:03:59 +0100

    feat : RoomOption, HotelOption, StayOption

--- src/structured_output/generation_data.py
+++ src/structured_output/generation_data.py
@@ -8,14 +8,34 @@ class Hotel(BaseModel):
     address: str
     tag: Literal["montagne", "plage", "ville", "campagne"]
 
+
 class Room(BaseModel):
     id: int 
     hotel_id: int 
     price: float
     capacity: int
 
+
 class Option(BaseModel):
     id: int 
     name: str 
     tag: Literal["hotel", "room", "stay"]
+    price: float
 
+
+class RoomOption(BaseModel):
+    id: int
+    room_id: int
+    option_id: int 
+
+
+class HotelOption(BaseModel):
+    id: int
+    hotel_id: int
+    option_id: int 
+
+
+class StayOption(BaseModel):
+    id: int
+    stay_id: int
+    option_id: int 
